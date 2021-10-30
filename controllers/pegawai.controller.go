d1b9d13b8cd2e681453a197523764de14d5170ee
Sahril Mahendra <sahrilmahendra@gmail.com>
2021-10-30 12:11:31 +0700

    menambahkan fitur update (PUT) pegawai

--- controllers/pegawai.controller.go
+++ controllers/pegawai.controller.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"echo-rest/models"
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
@@ -23,6 +24,24 @@ func StorePegawai(c echo.Context) error {
 	telepon := c.FormValue("telepon")
 
 	result, err := models.StorePegawai(nama, alamat, telepon)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, result)
+}
+
+func UpdatePegawai(c echo.Context) error {
+	id := c.FormValue("id")
+	nama := c.FormValue("nama")
+	alamat := c.FormValue("alamat")
+	telepon := c.FormValue("telepon")
+
+	convId, err := strconv.Atoi(id)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	result, err := models.UpdatePegawai(convId, nama, alamat, telepon)
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, err.Error())
 	}
--- models/pegawai.model.go
+++ models/pegawai.model.go
@@ -72,3 +72,33 @@ func StorePegawai(nama, alamat, telepon string) (Response, error) {
 
 	return res, nil
 }
+
+func UpdatePegawai(id int, nama, alamat, telepon string) (Response, error) {
+	var res Response
+	con := db.CreateCon()
+
+	sqlStatement := "UPDATE pegawai SET nama = ?, alamat = ?, telepon = ? WHERE id = ?"
+
+	stmt, err := con.Prepare(sqlStatement)
+	if err != nil {
+		return res, err
+	}
+
+	result, err := stmt.Exec(nama, alamat, telepon, id)
+	if err != nil {
+		return res, nil
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return res, nil
+	}
+
+	res.Status = http.StatusOK
+	res.Message = "Success"
+	res.Data = map[string]int64{
+		"rows_affected": rowsAffected,
+	}
+
+	return res, nil
+}
